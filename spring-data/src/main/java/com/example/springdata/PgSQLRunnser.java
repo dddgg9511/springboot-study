573a014a276b373f95d2f2bde776d74953ef28d7
ddgg9511@naver.com <ddgg9511@naver.com>
2022-03-19 12:39:46 +0900

    Spring data postgreSQL

--- spring-data/src/main/java/com/example/springdata/PgSQLRunnser.java
+++ spring-data/src/main/java/com/example/springdata/PgSQLRunnser.java
@@ -11,7 +11,7 @@ import java.sql.Connection;
 import java.sql.Statement;
 
 @Component
-public class MysqlRunner implements ApplicationRunner {
+public class PgSQLRunnser implements ApplicationRunner {
     @Autowired
     DataSource dataSource;
 
@@ -22,14 +22,16 @@ public class MysqlRunner implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         try(Connection connection = dataSource.getConnection()){
             System.out.println(dataSource.getClass());
+            System.out.println(dataSource.getConnection().getMetaData().getDriverName());
             System.out.println(connection.getMetaData().getURL());
             System.out.println(connection.getMetaData().getUserName());
 
             Statement statement = connection.createStatement();
-            String sql = "CREATE TABLE USER(ID INTEGER NOT NULL, name VARCHAR(256), PRIMARY KEY (id))";
+            String sql = "CREATE TABLE account(ID INTEGER NOT NULL, name VARCHAR(256), PRIMARY KEY (id))";
             statement.executeUpdate(sql);
         }
 
-        jdbcTemplate.execute("INSERT INTO USER VALUES (1,'choo')");;
+        jdbcTemplate.execute("INSERT INTO account VALUES (1,'choo')");;
     }
 }
+
