485b28f549e67b750c6a0ae54bbea27313530809
ddgg9511@naver.com <ddgg9511@naver.com>
2022-03-19 13:51:51 +0900

    jpa 연동

---
+++ spring-data/src/test/java/com/example/springdata/account/AccountRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.example.springdata.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@DataJpaTest
+class AccountRepositoryTest {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    public void di() throws SQLException {
+        try(Connection connection = dataSource.getConnection()){
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getURL());
+            System.out.println(metaData.getDriverName());
+            System.out.println(metaData.getUserName());
+        }
+    }
+
+    @Test
+    public void test(){
+        Account account = new Account();
+        account.setUsername("choo");
+        account.setPassword("pass");
+
+        Account newAccount = accountRepository.save(account);
+
+        assertThat(newAccount).isNotNull();
+
+
+        Optional<Account> existingAccount = accountRepository.findByUsername(newAccount.getUsername());
+        assertThat(existingAccount).isNotEmpty();
+
+        Optional<Account> nonExistingAccount = accountRepository.findByUsername("test");
+        assertThat(nonExistingAccount).isEmpty();
+    }
+
+}
